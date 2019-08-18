c5e3aa65cc63ff4cfb858bc0f59caf827f43e8c1
Silence <374619540@qq.com>
2019-08-18 18:33:11 +0800

    Update

--- llvm/MyPlugin/simple-plugin/MyPlugin.cpp
+++ llvm/MyPlugin/simple-plugin/MyPlugin.cpp
@@ -13,6 +13,25 @@ using namespace clang::ast_matchers;
 
 namespace MyPlugin {
     
+    /*
+     -ObjCInterfaceDecl 0x7fbed1029d90 <<invalid sloc>> <invalid sloc> implicit Protocol
+     |-TypedefDecl 0x7fbed102a0f8 <<invalid sloc>> <invalid sloc> implicit __NSConstantString 'struct __NSConstantString_tag'
+     | `-RecordType 0x7fbed1029f00 'struct __NSConstantString_tag'
+     |   `-Record 0x7fbed1029e60 '__NSConstantString_tag'
+     |-TypedefDecl 0x7fbed102a190 <<invalid sloc>> <invalid sloc> implicit __builtin_ms_va_list 'char *'
+     | `-PointerType 0x7fbed102a150 'char *'
+     |   `-BuiltinType 0x7fbed1029580 'char'
+     |-TypedefDecl 0x7fbed1062088 <<invalid sloc>> <invalid sloc> implicit __builtin_va_list 'struct __va_list_tag [1]'
+     | `-ConstantArrayType 0x7fbed1062030 'struct __va_list_tag [1]' 1
+     |   `-RecordType 0x7fbed1061ea0 'struct __va_list_tag'
+     |     `-Record 0x7fbed1061e00 '__va_list_tag'
+     |-ImportDecl 0x7fbed301b000 <./TestObj.h:9:1> col:1 implicit Foundation
+     |-ObjCInterfaceDecl 0x7fbed3033140 <line:13:1, line:15:2> line:13:12 test_obj
+     | |-super ObjCInterface 0x7fbed301b0f0 'NSObject'
+     | `-ObjCImplementation 0x7fbed3033270 'test_obj'
+     |-ObjCImplementationDecl 0x7fbed3033270 <TestObj.m:11:1, line:13:1> line:11:17 test_obj
+     */
+    
     // 语法树句柄
     class MyHandler : public MatchFinder::MatchCallback {
     private:
