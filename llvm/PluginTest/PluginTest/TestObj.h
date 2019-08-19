41fd5d7544db516c933041c2b2dad2397c9bf748
Silence <374619540@qq.com>
2019-08-19 23:44:52 +0800

    UpdateDemo

--- llvm/PluginTest/PluginTest/TestObj.h
+++ llvm/PluginTest/PluginTest/TestObj.h
@@ -12,6 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface test_obj : NSObject
 
+@property (strong, nonatomic) NSString *Test;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- llvm/PluginTest/PluginTest/main.m
+++ llvm/PluginTest/PluginTest/main.m
@@ -6,7 +6,6 @@
 //  Copyright © 2019年 Silence. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
