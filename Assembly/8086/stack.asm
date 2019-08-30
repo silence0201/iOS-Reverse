7f0815203ae75512ac7f89edbdf518595c178e09
Silence <374619540@qq.com>
2019-08-30 22:23:27 +0800

    stack

---
+++ Assembly/8086/stack.asm
@@ -0,0 +1,38 @@
+assume cs:code, ds:data, ss:stack
+
+; 栈段
+stack segment   
+    db 10 dup(8)
+stack ends 
+
+; 数据段
+data segment   
+    db 20 dup(9)
+data ends 
+
+; 代码段
+code segment
+start: 
+    mov ax, stack
+    mov ss, ax   
+    mov ax, data
+    mov ds, ax
+
+    mov ax, 1122h
+    mov bx, 3344h  
+    
+    mov [0], ax
+    
+    mov sp, 10 
+    push ax
+    push bx
+    pop ax
+    pop bx
+    
+    
+    ; 结束程序
+    mov ax, 4c00h
+    int 21h   
+code ends
+
+end start
